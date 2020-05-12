f0a5af5eb2d255e0793b7d9703040eb333fa6a77
Mauricio <mauricio.pinto@utec.edu.pe>
2020-05-12 00:15:58 -0500

    random file prototype

--- Makefile
+++ Makefile
@@ -2,6 +2,7 @@ CC=g++
 CFLAGS=-g
 ALLOBJ=main.o parser.o statichashing.o transactions.o ui.o randomfile.o record.o
 DEPS=parser.h statichashing.h transactions.h ui.h randomfile.h record.h
+DATAFILES=hash_data.bin hash_index.bin random_data.bin random_index.bin
 
 %.o: %.cpp $(DEPS)
 	$(CC) -c -o $@ $< $(CFLAGS)
@@ -13,3 +14,4 @@ myDBMS: $(ALLOBJ)
 clean:
 	rm -f myDBMS
 	rm -f $(ALLOBJ)
+	rm -f $(DATAFILES)
--- main.cpp
+++ main.cpp
@@ -1,8 +1,8 @@
 #include <iostream>
-#include "statichashing.h"
+#include "randomfile.h"
 
 int main(){
-	const char *filename = "hash_data.bin";
+	/*const char *filename = "hash_data.bin";
 	const char *hash_filename = "hash_table.bin";
 	hash_file h_file (filename, hash_filename, 6, 10);
 	
@@ -10,6 +10,16 @@ int main(){
 	init_record (r1, 1, "pepe");
 	h_file.add_record (r1);
 	record found = h_file.search_record (1);
-	cout << found.key << " " << found.value << endl;
+	cout << found.key << " " << found.value << endl;*/
+
+	/*const char *filename = "random_data.bin";
+	const char *index_filename = "random_index.bin";
+
+	random_file r_file (filename, index_filename);
+	record *r1 = new record;
+	init_record (r1, 1, "pepe");
+	r_file.add_record (r1);
+	record found = r_file.search_record (1);
+	cout << found.key << " " << found.value << endl;*/
 	return 0;
 }
